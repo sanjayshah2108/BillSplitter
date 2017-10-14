f073d996fd3f1b9733889000a35af62e4f5813a4
Sanjay Shah <Sanjay Shah>
2017-10-14 16:10:11 -0700

    Initial Commit

---
+++ BillSplitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillSplitter
+//
+//  Created by Sanjay Shah on 2017-10-14.
+//  Copyright © 2017 Sanjay Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
